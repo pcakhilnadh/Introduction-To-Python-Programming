a59f185375fb09a5306351bb092861156f94345a
Pc <akhilnp.is.17@nitj.ac.in>
2018-10-02 14:27:55 +0530

    updated

---
+++ ConditionalStatement/Exercise2.py
@@ -0,0 +1,9 @@
+def main():
+    food='Pizza'
+    if food == 'spam':
+        print('Ummmm, my favorite!')
+    else:
+        print("No, I won't have it. I want spam!")
+
+if __name__ == '__main__':
+    main()
