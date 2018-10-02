eefa7cad5d3744d5423faec7e89c973700d8bd8d
Pc <akhilnp.is.17@nitj.ac.in>
2018-10-02 15:12:12 +0530

    updated

---
+++ operators/Exercise6.py
@@ -0,0 +1,12 @@
+def main():
+    x=4
+    y=2
+    print("Bitwise AND X&Y=",        )
+    print("Bitwise OR X|Y=",        )
+    print("Bitwise XOR X^Y=",        )
+    print("Bitwise Complement ~X=",        )
+    print("Bitwise Left Shift X<<Y=",        )
+    print("Bitwise Right Shift X>>Y=",        )
+
+if __name__ == '__main__':
+    main()
