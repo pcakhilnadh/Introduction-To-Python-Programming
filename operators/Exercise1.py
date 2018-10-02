bc65056e61f41175195b4dc0cf0faeee65d24edc
Pc <akhilnp.is.17@nitj.ac.in>
2018-10-02 13:28:47 +0530

    excercise 1

---
+++ operators/Exercise1.py
@@ -0,0 +1,13 @@
+def main():
+    x=11
+    y=2
+    print("Addition X+Y=",        )
+    print("Subtraction  X-Y=",        )
+    print("Multiplication X*Y=",        )
+    print("Division X/Y=",        )
+    print("Modulus X%Y=",        )
+    print("Exponent X**Y=",        )
+    print("Floor Division X//Y=",        )
+
+if __name__ == '__main__':
+    main()
