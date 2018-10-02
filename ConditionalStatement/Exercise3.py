c1fe0930f848b742b9d4c552c6bb47b18ac85c57
Pc <akhilnp.is.17@nitj.ac.in>
2018-10-02 14:36:54 +0530

    update

---
+++ ConditionalStatement/Exercise3.py
@@ -0,0 +1,13 @@
+def main():
+    choice='z'
+    if choice == 'a':
+        print("You chose 'a'.")
+    elif choice == 'b':
+        print("You chose 'b'.")
+    elif choice == 'c':
+        print("You chose 'c'.")
+    else:
+        print("Invalid choice.")
+
+if __name__ == '__main__':
+    main()
---
+++ Solutions/Excercise1.py
@@ -0,0 +1,13 @@
+def main():
+    x=11
+    y=2
+    print("Addition X+Y=",x+y)
+    print("Subtraction  X-Y=",x-y)
+    print("Multiplication X*Y=",x*y)
+    print("Division X/Y=",x/y)
+    print("Modulus X%Y=",x%y)
+    print("Exponent X**Y=",x**y)
+    print("Floor Division X//Y=",x//y)
+
+if __name__ == '__main__':
+    main()
