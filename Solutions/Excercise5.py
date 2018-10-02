784ff48baf63ea65f8ec8cf0232e0eb1d07a837b
Pc <akhilnp.is.17@nitj.ac.in>
2018-10-02 15:01:53 +0530

    updated

---
+++ Solutions/Excercise5.py
@@ -0,0 +1,13 @@
+def main():
+    x=78
+    if(x>=90):
+        print("Eligible For Research")
+    elif (x>70) and (x<90):
+        if(x>75):
+            print("Eligible for Placement")
+        print("Distinction")
+    else:
+        print("Fail")
+
+if __name__ == '__main__':
+    main()
---
+++ operators/Exercise5.py
@@ -0,0 +1,13 @@
+def main():
+    x=78
+    if():
+        print("Eligible For Research")
+    elif () ___ (x<90):
+        if():
+            print("Eligible for Placement")
+        print("Distinction")
+    else:
+        print("Fail")
+
+if __name__ == '__main__':
+    main()
