e6a3348b4773a85edd18b7abf18733569fffa7b3
Pc <akhilnp.is.17@nitj.ac.in>
2018-10-02 13:59:45 +0530

    update

---
+++ ConditionalStatement/Exercise1.py
@@ -0,0 +1,10 @@
+def main():
+    food = 'spam'
+
+    if food == 'spam':
+        print('Ummmm, my favorite!')
+        print('I feel like saying it 100 times...')
+        print(100 * (food + '! '))
+
+if __name__ == '__main__':
+    main()
