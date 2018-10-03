4ddbd40c94aef1a3d54dcfa94d531e3fafacf1a8
Pc <akhilnp.is.17@nitj.ac.in>
2018-10-03 21:10:32 +0530

    updated

---
+++ Exercise/Exercise8.py
@@ -0,0 +1,18 @@
+'''
+Enter a number : 5
+Required Output = factorial of 5 is : 120
+fill the <x> with your code
+5 fill in the blanks
+'''
+ <1> factorial(number):
+    if (number<=1):
+        return 1
+    else:
+        return (<2>)
+
+def main():
+    inputNum = <3>(input("Enter a Number : "))
+    print("factorial of "+<4>+" is :",<5>)
+
+if __name__ == '__main__':
+    main()
